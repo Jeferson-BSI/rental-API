8c0eac9639488a815d2b013899096542b41760ea
jeferson-bsi <jefersoncou18@gmail.com>
2022-03-02 21:32:03 -0300

    Devolutions rentals

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -15,6 +15,15 @@ class DayjsDateProvider implements IDateProvider {
     return compareDate;
   }
 
+  compareInDays(start_date: Date, end_date: Date): number {
+    const end_date_utc = this.convertToUtc(end_date);
+    const start_date_utc = this.convertToUtc(start_date);
+
+    const compareDate = dayjs(end_date_utc).diff(start_date_utc, 'days');
+
+    return compareDate;
+  }
+
   convertToUtc(date: Date): string {
     return dayjs(date).utc().local().format();
   }
--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,13 +1,21 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRentalUseCase/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 
 import ensureAuthenticate from '../middlewares/ensureAuthenticate';
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalsRoutes.post('/', ensureAuthenticate, createRentalController.handle);
+
+rentalsRoutes.post(
+  '/devolution/:id',
+  ensureAuthenticate,
+  devolutionRentalController.handle
+);
 
 export { rentalsRoutes };
